a6b4ef900448917a9936c955ed0d4be1fcb4c516
MonicaRafaat13 <162500577+MonicaRafaat13@users.noreply.github.com>
2025-05-08 14:19:13 +0300

    Update SignUpLogin.java

--- SignUpLogin.java
+++ SignUpLogin.java
@@ -1,4 +1,3 @@
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,6 +6,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+// this class make a GUI to make user can login or sign up
 class User {
     String username;
     String ID;
@@ -30,10 +30,7 @@ public class SignUpLogin {
         showLoginPage();
     }
 
-
-
-
-
+/* =========================================== LOGIN PAGE ========================================== */
     private void showLoginPage() {
         frame = new JFrame("Login");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -51,25 +48,29 @@ public class SignUpLogin {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
     
+        // create username Label
         gbc.gridx = 0;
         gbc.gridy = 0;
         JLabel usernameLabel = new JLabel("Username:");
         usernameLabel.setFont(new Font("Arial", Font.BOLD, 18));
         formPanel.add(usernameLabel, gbc);
-    
+
+        // create username Field
         gbc.gridx = 1;
         gbc.gridy = 0;
         JTextField usernameField = new JTextField(20);
         usernameField.setFont(new Font("Arial", Font.PLAIN, 18));
         usernameField.setPreferredSize(new Dimension(250, 30));
         formPanel.add(usernameField, gbc);
-    
+        
+        // create Password Label
         gbc.gridx = 0;
         gbc.gridy = 1;
         JLabel passwordLabel = new JLabel("Password:");
         passwordLabel.setFont(new Font("Arial", Font.BOLD, 18));
         formPanel.add(passwordLabel, gbc);
     
+        // create Password Field Label
         gbc.gridx = 1;
         gbc.gridy = 1;
         JPasswordField passwordField = new JPasswordField(20);
@@ -77,9 +78,12 @@ public class SignUpLogin {
         passwordField.setPreferredSize(new Dimension(250, 30));
         formPanel.add(passwordField, gbc);
     
+        //Create Login button
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout());
         JButton loginButton = new JButton("Login");
+
+        // Action listener for login button
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -99,25 +103,30 @@ public class SignUpLogin {
                 JOptionPane.showMessageDialog(null, "Invalid username or password");
             }
         });
+
+        // Style for login button
         loginButton.setBackground(Color.decode("#03A9F4"));
         loginButton.setForeground(Color.WHITE);
         loginButton.setFont(new Font("Arial", Font.BOLD, 18));
         loginButton.setPreferredSize(new Dimension(120, 40));
     
         buttonPanel.add(loginButton);
-    
+
+        // Position for login button
         gbc.gridx = 0;
         gbc.gridy = 2;
         gbc.gridwidth = 2;
         formPanel.add(buttonPanel, gbc);
-    
+
+        // create sign up button
         JPanel signUpPanel = new JPanel();
         signUpPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 0));
         JLabel signUpLabel = new JLabel("Don't have an account?");
         signUpLabel.setFont(new Font("Arial", Font.PLAIN, 14));
         signUpPanel.add(signUpLabel);
-    
         JButton signUpButton = new JButton("Sign Up");
+
+        // action listener for sign up button
         signUpButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -125,12 +134,15 @@ public class SignUpLogin {
                 showSignUpPage();
             }
         });
+
+        //Style for sign up button
         signUpButton.setBackground(Color.decode("#4CAF50"));
         signUpButton.setForeground(Color.WHITE);
         signUpButton.setFont(new Font("Arial", Font.BOLD, 14));
         signUpButton.setPreferredSize(new Dimension(90, 30));
         signUpPanel.add(signUpButton);
-    
+        
+        // position for sign up button
         gbc.gridx = 0;
         gbc.gridy = 3;
         gbc.gridwidth = 2;
@@ -142,7 +154,7 @@ public class SignUpLogin {
         frame.setVisible(true);
     }
     
-
+/* =========================================== SIGN UP PAGE ========================================== */
     private void showSignUpPage() {
         frame = new JFrame("Sign Up");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -160,134 +172,143 @@ public class SignUpLogin {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
         
+        // create username label
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        formPanel.add(usernameLabel, gbc);
 
-    gbc.gridx = 0;
-    gbc.gridy = 0;
-    JLabel usernameLabel = new JLabel("Username:");
-    usernameLabel.setFont(new Font("Arial", Font.BOLD, 18));
-    formPanel.add(usernameLabel, gbc);
+        // create username Field
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        JTextField usernameField = new JTextField(20);
+        usernameField.setFont(new Font("Arial", Font.PLAIN, 18));
+        usernameField.setPreferredSize(new Dimension(250, 30));
+        formPanel.add(usernameField, gbc);
 
-    gbc.gridx = 1;
-    gbc.gridy = 0;
-    JTextField usernameField = new JTextField(20);
-    usernameField.setFont(new Font("Arial", Font.PLAIN, 18));
-    usernameField.setPreferredSize(new Dimension(250, 30));
-    formPanel.add(usernameField, gbc);
+        // create ID label
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        JLabel IDLabel = new JLabel("ID:");
+        IDLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        formPanel.add(IDLabel, gbc);
 
-    gbc.gridx = 0;
-    gbc.gridy = 1;
-    JLabel IDLabel = new JLabel("ID:");
-    IDLabel.setFont(new Font("Arial", Font.BOLD, 18));
-    formPanel.add(IDLabel, gbc);
+        // create ID Field
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        JTextField IDField = new JTextField(20);
+        IDField.setFont(new Font("Arial", Font.PLAIN, 18));
+        IDField.setPreferredSize(new Dimension(250, 30));
+        formPanel.add(IDField, gbc);
 
-    gbc.gridx = 1;
-    gbc.gridy = 1;
-    JTextField IDField = new JTextField(20);
-    IDField.setFont(new Font("Arial", Font.PLAIN, 18));
-    IDField.setPreferredSize(new Dimension(250, 30));
-    formPanel.add(IDField, gbc);
+        // Create Email label
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        JLabel emailLabel = new JLabel("Email:");
+        emailLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        formPanel.add(emailLabel, gbc);
 
-    gbc.gridx = 0;
-    gbc.gridy = 2;
-    JLabel emailLabel = new JLabel("Email:");
-    emailLabel.setFont(new Font("Arial", Font.BOLD, 18));
-    formPanel.add(emailLabel, gbc);
+        // Create Email Field
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        JTextField emailField = new JTextField(20);
+        emailField.setFont(new Font("Arial", Font.PLAIN, 18));
+        emailField.setPreferredSize(new Dimension(250, 30));
+        formPanel.add(emailField, gbc);
 
-    gbc.gridx = 1;
-    gbc.gridy = 2;
-    JTextField emailField = new JTextField(20);
-    emailField.setFont(new Font("Arial", Font.PLAIN, 18));
-    emailField.setPreferredSize(new Dimension(250, 30));
-    formPanel.add(emailField, gbc);
+        // Create Password label
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        formPanel.add(passwordLabel, gbc);
 
-    gbc.gridx = 0;
-    gbc.gridy = 3;
-    JLabel passwordLabel = new JLabel("Password:");
-    passwordLabel.setFont(new Font("Arial", Font.BOLD, 18));
-    formPanel.add(passwordLabel, gbc);
+        // Create Password Field
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        JPasswordField passwordField = new JPasswordField(20);
+        passwordField.setFont(new Font("Arial", Font.PLAIN, 18));
+        passwordField.setPreferredSize(new Dimension(250, 30));
+        formPanel.add(passwordField, gbc);
 
-    gbc.gridx = 1;
-    gbc.gridy = 3;
-    JPasswordField passwordField = new JPasswordField(20);
-    passwordField.setFont(new Font("Arial", Font.PLAIN, 18));
-    passwordField.setPreferredSize(new Dimension(250, 30));
-    formPanel.add(passwordField, gbc);
+        // Create Confirm Password label
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        JLabel ConfirmpasswordLabel = new JLabel("Confirm Password:");
+        ConfirmpasswordLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        formPanel.add(ConfirmpasswordLabel, gbc);
 
-    gbc.gridx = 0;
-    gbc.gridy = 4;
-    JLabel ConfirmpasswordLabel = new JLabel("Confirm Password:");
-    ConfirmpasswordLabel.setFont(new Font("Arial", Font.BOLD, 18));
-    formPanel.add(ConfirmpasswordLabel, gbc);
+        // Create Confirm Password Field
+        gbc.gridx = 1;
+        gbc.gridy = 4;
+        JPasswordField ConfirmPasswordField = new JPasswordField(20);
+        ConfirmPasswordField.setFont(new Font("Arial", Font.PLAIN, 18));
+        ConfirmPasswordField.setPreferredSize(new Dimension(250, 30));
+        formPanel.add(ConfirmPasswordField, gbc);
 
-    gbc.gridx = 1;
-    gbc.gridy = 4;
-    JPasswordField ConfirmPasswordField = new JPasswordField(20);
-    ConfirmPasswordField.setFont(new Font("Arial", Font.PLAIN, 18));
-    ConfirmPasswordField.setPreferredSize(new Dimension(250, 30));
-    formPanel.add(ConfirmPasswordField, gbc);
+        // create sign up button
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+        JButton signUpButton = new JButton("Sign Up");
 
-    
+        // action listener for sign up button
+        signUpButton.addActionListener(new ActionListener() {
+        @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+                String confirmPassword = new String(ConfirmPasswordField.getPassword());
+                String email = emailField.getText();
+                String ID = IDField.getText();
 
-    JPanel buttonPanel = new JPanel();
-    buttonPanel.setLayout(new FlowLayout());
-    JButton signUpButton = new JButton("Sign Up");
+                if (!password.equals(confirmPassword)) {        // make sure confirm password = password
+                    JOptionPane.showMessageDialog(null, "Passwords do not match");
+                    return;
+                }
+                if (!email.contains("@")) {                    // make sure email contain '@'
+                    JOptionPane.showMessageDialog(null, "Invalid email address");
+                    return;
+                }
 
-    signUpButton.addActionListener(new ActionListener() {
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String username = usernameField.getText();
-        String password = new String(passwordField.getPassword());
-        String confirmPassword = new String(ConfirmPasswordField.getPassword());
-        String email = emailField.getText();
-        String ID = IDField.getText();
+                // make sure username is unique
+                for (User user : users) {
+                    if (user.username.equals(username)) {
+                        JOptionPane.showMessageDialog(null, "Username already exists");
+                        return;
+                    }
+                }
 
-        if (!password.equals(confirmPassword)) {
-            JOptionPane.showMessageDialog(null, "Passwords do not match");
-            return;
-        }
-        if (!email.contains("@")) {
-            JOptionPane.showMessageDialog(null, "Invalid email address");
-            return;
-        }
-
-
-        for (User user : users) {
-            if (user.username.equals(username)) {
-                JOptionPane.showMessageDialog(null, "Username already exists");
-                return;
+                User user = new User(username, password, email, ID);
+                users.add(user);
+                saveUsers();
+                JOptionPane.showMessageDialog(null, "Sign up successful");
+                frame.dispose();
+                showLoginPage();
             }
-        }
+        });
+        
+        //Style for sign up button
+        signUpButton.setBackground(Color.decode("#4CAF50"));
+        signUpButton.setForeground(Color.WHITE);
+        signUpButton.setFont(new Font("Arial", Font.BOLD, 18));
+        signUpButton.setPreferredSize(new Dimension(120, 40));
 
-        User user = new User(username, password, email, ID);
-        users.add(user);
-        saveUsers();
-        JOptionPane.showMessageDialog(null, "Sign up successful");
-        frame.dispose();
-        showLoginPage();
+        buttonPanel.add(signUpButton);
+
+        // position for sign up button
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        gbc.gridwidth = 2;
+        formPanel.add(buttonPanel, gbc);
+
+        frame.add(formPanel, BorderLayout.CENTER);
+        frame.setSize(600, 550);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
-    });
-    
-    signUpButton.setBackground(Color.decode("#4CAF50"));
-    signUpButton.setForeground(Color.WHITE);
-    signUpButton.setFont(new Font("Arial", Font.BOLD, 18));
-    signUpButton.setPreferredSize(new Dimension(120, 40));
-
-    
-    buttonPanel.add(signUpButton);
-
-    gbc.gridx = 0;
-    gbc.gridy = 5;
-    gbc.gridwidth = 2;
-    formPanel.add(buttonPanel, gbc);
-
-    frame.add(formPanel, BorderLayout.CENTER);
-    frame.setSize(600, 550);
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
-    }
-
-    private void loadUsers() {
+    private void loadUsers() {             // Load users from file
         try (BufferedReader reader = new BufferedReader(new FileReader("users.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -302,7 +323,7 @@ public class SignUpLogin {
         }
     }    
     
-    private void saveUsers() {
+    private void saveUsers() {          // save users in file (users.txt)
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("users.txt"))) {
             for (User user : users) {
                 writer.write(user.username + "," + user.ID  + "," + user.email + "," + user.password );
@@ -313,12 +334,9 @@ public class SignUpLogin {
         }
     }
 
+    // Main
     public static void main(String[] args) {
         new SignUpLogin();
     }
     
 }
-
-    
-
-
